4c83cd9072dd758eca485930cf5d814a95d1ed1c
Protik Das <protikdas@hotmail.com>
2017-10-02 16:07:19 -0400

    Added rotating animation

--- protick/index.js
+++ protick/index.js
@@ -3,5 +3,13 @@ $(document).ready(function() {
 		var comment = $('.commentBox').val();
 		$('<li>').text(comment).prependTo('.comment-list');
 		$('.commentBox').val('');
-    });
+		});
+		
+	$("img.img-band").mouseenter(function(){
+			$(this).addClass("change").delay(5000).queue(function(){
+				$(this).removeClass("change").dequeue();
+			});
+			
+	});
 });
+
